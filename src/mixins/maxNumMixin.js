82393b4ddf7722a0359d591a5a4f7ceac121be2b
malin <malin01@jiuqi.com.cn>
2019-06-05 12:30:17 +0800

    data改变时代码

--- src/mixins/maxNumMixin.js
+++ src/mixins/maxNumMixin.js
@@ -13,11 +13,23 @@ export default {
     }
   },
   watch: {
-    'data.length' (val) {
-      this.dataLength = val
-      this.updataShowList(val)
-      // 是否隐藏向左向右箭头
-      this.isHideLRIcon()
+    data: {
+      deep: true,
+      handler: function (val, oldval) {
+        if (this.dataLength !== val.length) {
+          this.dataLength = val.length
+          this.updataShowList()
+          // 是否隐藏向左向右箭头
+          this.isHideLRIcon()
+        } else {
+          // 数量不变，数据更新，记录offset，更新数据，设置offset
+          // TODO（还需要修改，如果内容长度改变很大，offset也记录不准确）
+          const recordOffset = this.getCurrentScrollOffset(this.isHorizontal ? 1 : 2)
+          this.setOffset(0, 0)
+          this.changeShowList(this.showList[0].id)
+          this.isHorizontal ? this.setOffset(recordOffset, 0) : this.setOffset(0, recordOffset)
+        }
+      }
     },
     hideDirectionBar () {
       this.isHideLRIcon()
@@ -36,9 +48,9 @@ export default {
     /**
      * 更新显示的list
      */
-    updataShowList (val) {
+    updataShowList () {
       // data数量改变，showList重置，如果要定位请设置activeTab
-      this.showList = this.dataLength <= this.maxnum ? this.data : val.slice(0, this.maxnum)
+      this.showList = this.dataLength <= this.maxnum ? this.data : this.data.slice(0, this.maxnum)
       this.scrollToActiveTab()
     },
     /**

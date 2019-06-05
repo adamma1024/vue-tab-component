95796518a2fb55541e60ccf070dbb4aa9ed6ec38
malin <malin01@jiuqi.com.cn>
2019-06-05 15:55:22 +0800

    修改offset

--- src/mixins/offsetMixin.js
+++ src/mixins/offsetMixin.js
@@ -41,17 +41,16 @@ export default {
 
       if (!currentOffset && this.beginPos === 0) return
 
-      let newOffset =
-        currentOffset > containerWidth ? currentOffset - containerWidth : 0
+      let newOffset = currentOffset - 50
+      // currentOffset > containerWidth ? currentOffset - containerWidth : 0
 
-      if (this.beginPos >= 0) {
-        if (this.beginPos >= 8) {
-          this.showList = this.data.slice(this.beginPos - 8, this.beginPos + this.maxnum - 8)
-          newOffset = currentOffset - 50
-        } else {
-          this.showList = this.data.slice(0, this.maxnum)
-        }
+      if (this.beginPos >= 8) {
+        this.showList = [...this.data.slice(this.beginPos - 8, this.beginPos + this.maxnum - 8)]
+        // newOffset = currentOffset - 50
+      } else {
+        this.showList = [...this.data.slice(0, this.maxnum)]
       }
+
       this.isHorizontal
         ? this.setOffset(newOffset, 0)
         : this.setOffset(0, newOffset)
@@ -71,18 +70,16 @@ export default {
         : this.getCurrentScrollOffset(2)
       if (navWidth - currentOffset <= containerWidth && this.showList[this.showList.length - 1].id === this.data[this.dataLength - 1].id) return
 
-      let newOffset =
-      navWidth - currentOffset > containerWidth * 2
-        ? currentOffset + containerWidth
-        : navWidth - containerWidth
+      let newOffset = currentOffset + 50
+      // navWidth - currentOffset > containerWidth * 2
+      //   ? currentOffset + containerWidth
+      //   : navWidth - containerWidth
 
-      if (this.beginPos < this.dataLength - this.maxnum) {
-        if (this.dataLength - this.maxnum - this.beginPos >= 8) {
-          this.showList = this.data.slice(this.beginPos + 8, this.beginPos + this.maxnum + 8)
-          newOffset = currentOffset + 50
-        } else {
-          this.showList = this.data.slice(this.dataLength - this.maxnum, this.dataLength)
-        }
+      if (this.dataLength - this.maxnum - this.beginPos >= 0) {
+        this.showList = [...this.data.slice(this.beginPos + 5, this.beginPos + this.maxnum + 5)]
+        // newOffset = currentOffset + 50
+      } else {
+        this.showList = [...this.data.slice(this.dataLength - this.maxnum, this.dataLength)]
       }
 
       this.isHorizontal
